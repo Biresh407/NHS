d3020f78bc1e4a1ef9c116c63ca3a6a148167fc3
Brijesh Singh <Brijesh Singh@DESKTOP-6S1DA1G>
2019-12-31 00:55:13 +0530

    commiting change to branch NHS

--- src/main/java/com/nhs/patient/info/Alternative_String_Merge.java
+++ src/main/java/com/nhs/patient/info/Alternative_String_Merge.java
@@ -1,9 +1,41 @@
 package com.nhs.patient.info;
 
+import java.util.Scanner;
+
 public class Alternative_String_Merge {
 
+	static String mix(String str1, String str2) {
+		int i = 0, j = 0;
+		String str = "";
+		while (i < str1.length() && j < str2.length()) {
+			str = str + str1.charAt(i++);
+			str = str + str2.charAt(j++);
+		}
+		while (i < str1.length()) {
+			str = str + str1.charAt(i++);
+		}
+		while (j < str2.length()) {
+			str = str + str2.charAt(j++);
+		}
+		return str;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = null;
+		String str1 = null;
+		String str2 = null;
+		
+		//scanner object
+		sc = new Scanner(System.in);
+		System.out.println("Enter ur String:");
+		str1 = sc.nextLine();
+		str2 = sc.nextLine();
+		
+		// call method
+		String result = Alternative_String_Merge.mix(str1, str2);
+		System.out.println("Final Result:" + result);
+
+		sc.close();
 
 	}
 
